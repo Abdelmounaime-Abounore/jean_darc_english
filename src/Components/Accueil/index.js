0c4dd8ca52842b4b345ba7b7ff76c607fd759411
Abdelmounaime-Abounore <abdelmounaime.abounore@gmail.com>
2023-06-15 17:01:34 +0100

    add footer

--- src/Components/Accueil/index.js
+++ src/Components/Accueil/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo_Misssion from '../../assets/images/LogoMission.png'
+import Logo from '../../assets/images/logo.jpg'
 import Video_about from '../../assets/images/aboutexample.mp4'
 import Cambridge from '../../assets/images/Cambridge.png'
 import Bsologo from '../../assets/images/BSOLogo.png'
@@ -17,6 +18,7 @@ import  Care from '../../assets/images/Care.png'
 import  Curiosity from '../../assets/images/Curiosity.png'
 import  creativity from '../../assets/images/Creativity.png'
 import './index.scss';
+import { Link } from 'react-router-dom';
 
 
 const Accueil = () => {
@@ -186,6 +188,51 @@ const Accueil = () => {
                     </div>
                 </div>
             </div>
+        </div>
+
+        {/* footer */}
+
+        <div className="footer">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-4 footer-logo">
+                        <img src={Logo}></img>
+                    </div>
+                    <div className="col-lg-2 footer-elements">
+                        <h6 className='sitemap'>SITEMAP</h6>
+                        <p>
+                            <Link>Admissions</Link>
+                        </p>
+                        <p>
+                            <Link>Gallery</Link>
+                        </p>
+                        <p>
+                            <Link>Carreer</Link>
+                        </p>
+                        <p>
+                            <Link>Enroll now</Link>
+                        </p>
+                        <p>
+                            <Link>School Policies</Link>
+                        </p>
+                    </div>
+                    <div className='col-lg-3 footer-elements'> 
+                        <h6 className='contact-datails'>CONTACT DETAILS</h6>
+                        <p>Jeanne D’Arc International School N°57 Boulevard Moulay Youssef, Anfa - Casablanca</p>
+                        <p>+212 522 22 01 70   -   +212 642 56 23 28</p>
+                        <p>contact@jeanned’arcinternational.ma</p>
+                    </div>
+                    <div className="col-lg-3 footer-elements">
+                        <h6 className='contact-us'>CONNECT WHITH US</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {/* Copyright */}
+
+        <div className='copy-right'>
+            <p>ALL RIGHTS RESERVED  © 2023  - JEANNE D’ARC INTERNATIONAL SCHOOL</p>
         </div>
     </div>
 
